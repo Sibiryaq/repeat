7d5890b3ac0de966a1b00977859a9405447d1f46
Sibiryaq <4totakoeaaa@mail.ru>
2024-12-16 02:12:13 +0700

    Java MultiThreading(Race condition) 16.12T01:57

--- src/multuthreading/RaceConditionExample.java
+++ src/multuthreading/RaceConditionExample.java
@@ -1,20 +1,28 @@
 package multuthreading;
 
+import java.util.concurrent.CountDownLatch;
+
 public class RaceConditionExample {
     static int counter = 0;
-    static boolean condition = false;
+    //static boolean condition = false;
+
+    static CountDownLatch latch = new CountDownLatch(1);
 
     public static void main(String[] args) throws InterruptedException {
         Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 100; i++) {
-                if(condition) {
+            try {
+                latch.await();
+                for (int i = 0; i < 100; i++) {
                     counter++;
                 }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
+
         });
 
-        Thread thread2 = new Thread( () -> {
-            condition = true;
+        Thread thread2 = new Thread(() -> {
+            latch.countDown();  //уменьшает счётчик latch, сигнализирую потоку thread1 выполнение
         });
 
         thread1.start();
@@ -28,6 +36,6 @@ public class RaceConditionExample {
         thread1.join(); // заставим main подождать выполнения наших потоков
         thread2.join();
 
-        System.out.println(counter);
+        System.out.println(counter); // всегда будет выводить 100
     }
 }
